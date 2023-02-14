fa797ba02e5c0cc1226632251aa2d71796e162df
«karina» <vlasova_karina1985@mail.ru>
2023-02-14 20:20:06 +0300

    SolveTask

---
+++ Task.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. 
+
+string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] arraay2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
